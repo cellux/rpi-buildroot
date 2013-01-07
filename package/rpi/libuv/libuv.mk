4c2743e88e085277dedb7c25ab82787e0a5f469c
Ruzsa Balazs <ruzsa.balazs@gmail.com>
2013-01-07 11:39:49 +0100

    pass -Iinclude -Iinclude/uv-private -Isrc to libuv build ONLY
    adding the options to TARGET_CPPFLAGS affected all packages
    
    + there is no need to install libuv to staging

--- package/rpi/libuv/libuv.mk
+++ package/rpi/libuv/libuv.mk
@@ -7,18 +7,8 @@ LIBUV_VERSION = node-v0.9.4
 LIBUV_SITE = https://github.com/joyent/libuv/archive
 LIBUV_SOURCE = $(LIBUV_VERSION).tar.gz
 
-LIBUV_INSTALL_STAGING = YES
-TARGET_CPPFLAGS += -Iinclude -Iinclude/uv-private -Isrc
-
 define LIBUV_BUILD_CMDS
-	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) libuv.so
-endef
-
-define LIBUV_INSTALL_STAGING_CMDS
-	$(INSTALL) -m 0644 -D $(@D)/include/uv.h $(STAGING_DIR)/usr/include/uv.h
-	$(INSTALL) -m 0755 -d $(STAGING_DIR)/usr/include/uv-private
-	$(INSTALL) -m 0644 $(@D)/include/uv-private/*.h $(STAGING_DIR)/usr/include/uv-private
-	$(INSTALL) -m 0644 $(@D)/libuv.so $(STAGING_DIR)/usr/lib/libuv.so
+	$(TARGET_MAKE_ENV) $(MAKE) $(TARGET_CONFIGURE_OPTS) CFLAGS="$(TARGET_CFLAGS) -Iinclude -Iinclude/uv-private -Isrc" -C $(@D) libuv.so
 endef
 
 define LIBUV_INSTALL_TARGET_CMDS
