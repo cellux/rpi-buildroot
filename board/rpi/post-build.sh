ae7b64c32e5460019d14ce40aa26ea7f4b1107ef
Ruzsa Balazs <cellux@omkamra.hu>
2013-01-08 20:12:06 +0100

    fix /etc/shadow and /etc/fstab

--- board/rpi/post-build.sh
+++ board/rpi/post-build.sh
@@ -8,9 +8,11 @@ install -T -m 0644 $BR_ROOT/output/images/zImage $BR_ROOT/output/staging/boot/ke
 tar -C $BR_ROOT/output/staging/boot -cvzf $BR_ROOT/output/images/bootfs.tar.gz --owner 0 --group 0 .
 
 # set root password to `passpass'
-sed -i -e 's#^root:.*:#root:paTMTLhmeVJck:#' $TARGETDIR/etc/shadow
+install -T -m 0600 $BR_ROOT/system/skeleton/etc/shadow $TARGETDIR/etc/shadow
+sed -i -e 's#^root:[^:]*:#root:paTMTLhmeVJck:#' $TARGETDIR/etc/shadow
 
 # mount /boot
+install -T -m 0644 $BR_ROOT/system/skeleton/etc/fstab $TARGETDIR/etc/fstab
 echo '/dev/mmcblk0p1 /boot vfat defaults 0 0' >> $TARGETDIR/etc/fstab
 
 # remove leftover junk
