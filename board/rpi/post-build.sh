ef90317f7d55d3d53b18642424cfc9adb3e9c5c4
Ruzsa Balazs <cellux@omkamra.hu>
2013-01-09 06:28:40 +0100

    use rpi-3.6.y kernel, disabled some packages, create /boot in target

--- board/rpi/post-build.sh
+++ board/rpi/post-build.sh
@@ -1,15 +1,18 @@
 TARGETDIR=$1
 BR_ROOT=$PWD
 
-# copy kernel image to /boot
+# copy kernel image to staging /boot
 install -T -m 0644 $BR_ROOT/output/images/zImage $BR_ROOT/output/staging/boot/kernel.img
 
-# create boot.tar.gz
+# create boot.tar.gz from contents of staging /boot
 tar -C $BR_ROOT/output/staging/boot -cvzf $BR_ROOT/output/images/bootfs.tar.gz --owner 0 --group 0 .
 
 # set root password to `passpass'
 install -T -m 0600 $BR_ROOT/system/skeleton/etc/shadow $TARGETDIR/etc/shadow
 sed -i -e 's#^root:[^:]*:#root:paTMTLhmeVJck:#' $TARGETDIR/etc/shadow
+
+# create an empty /boot directory in target
+install -d -m 0755 $TARGETDIR/boot
 
 # mount /boot
 install -T -m 0644 $BR_ROOT/system/skeleton/etc/fstab $TARGETDIR/etc/fstab
