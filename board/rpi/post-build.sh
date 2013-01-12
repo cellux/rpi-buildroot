9f552a336241c03222d1d7cbed27aae9999b13f1
Ruzsa Balazs <cellux@omkamra.hu>
2013-01-12 11:37:16 +0100

    enable portmap (needed for userspace nfs mounts), symlink /etc/resolv.conf to /proc/net/pnp

--- board/rpi/post-build.sh
+++ board/rpi/post-build.sh
@@ -12,3 +12,6 @@ install -d -m 0755 $TARGETDIR/boot
 install -T -m 0644 $BR_ROOT/system/skeleton/etc/fstab $TARGETDIR/etc/fstab
 echo '/dev/mmcblk0p1 /boot vfat defaults 0 0' >> $TARGETDIR/etc/fstab
 
+# link /etc/resolv.conf to /proc/net/pnp
+rm -f $TARGETDIR/etc/resolv.conf
+ln -s /proc/net/pnp $TARGETDIR/etc/resolv.conf
