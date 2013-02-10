fc208091afad5aea928b41c4f449ccbf5c531733
Ruzsa Balazs <cellux@omkamra.hu>
2013-02-10 15:31:43 +0100

    chmod rpi shared libs to 0755 when installing to target

--- package/rpi/rpi-userland/rpi-userland.mk
+++ package/rpi/rpi-userland/rpi-userland.mk
@@ -9,7 +9,7 @@ RPI_USERLAND_SOURCE = $(RPI_USERLAND_VERSION).tar.gz
 RPI_USERLAND_INSTALL_TARGET = YES
 
 define RPI_USERLAND_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0644 $(@D)/build/lib/*.so $(TARGET_DIR)/usr/lib
+	$(INSTALL) -m 0755 $(@D)/build/lib/*.so $(TARGET_DIR)/usr/lib
 	$(INSTALL) -m 0755 $(@D)/build/bin/* $(TARGET_DIR)/usr/bin
 endef
 
